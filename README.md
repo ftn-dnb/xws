4da958c8a463377ee04d3b867d03f094d58fe73f
Boris Sulicenko <b.sulicenko@gmail.com>
2019-10-20 15:08:19 +0200

    Init SpringBoot project

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# xws
+# XML i web servisi
+Projekat iz predmeta XML i web servisi.  
+Tim 05
+
+Članovi tima:
+* Boris Šuličenko SW 4/2016
+* David Stanojević SW 41/2016
+* Nemanja Miković SW 68/2016
---
+++ xws-service/src/test/java/rs/ac/uns/ftn/xwsservice/XwsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.xwsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XwsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
