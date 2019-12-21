c584452775cc3abeab849b846c1e12c0635901ea
Boris Sulicenko <b.sulicenko@gmail.com>
2019-12-21 19:40:06 +0100

    Manje izmene

--- xws-service/src/main/java/rs/ac/uns/ftn/xwsservice/controller/DOMParserController.java
+++ xws-service/src/main/java/rs/ac/uns/ftn/xwsservice/controller/DOMParserController.java
@@ -1,8 +1,6 @@
 package rs.ac.uns.ftn.xwsservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
