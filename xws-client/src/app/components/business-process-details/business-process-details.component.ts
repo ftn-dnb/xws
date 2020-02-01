ddd2599b5de02bbbef433152b81d1efc5d5864a2
boris-ns <b.sulicenko@gmail.com>
2020-02-01 16:54:40 +0100

    Napravljena je forma na frontendu za dodavanje recenzenata

--- xws-client/src/app/components/business-process-details/business-process-details.component.ts
+++ xws-client/src/app/components/business-process-details/business-process-details.component.ts
@@ -2,6 +2,7 @@ import { ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { BusinessProcessService } from './../../services/business-process.service';
 import { Component, OnInit } from '@angular/core';
+import { elementEventFullName } from '@angular/compiler/src/view_compiler/view_compiler';
 
 @Component({
   selector: 'app-business-process-details',
@@ -12,6 +13,8 @@ export class BusinessProcessDetailsComponent implements OnInit {
 
   private processId: string = '';
   process = {};
+  possibleReviewers: any[] = [];
+  choosenReviewers: any[] = [];
 
   constructor(private businessProcessService: BusinessProcessService, 
               private toastr: ToastrService, 
@@ -21,13 +24,52 @@ export class BusinessProcessDetailsComponent implements OnInit {
 
   ngOnInit() {
     this.getProcessData();
+    this.getPossibleReviewers();
   }
 
   private getProcessData(): void {
     this.businessProcessService.getProcess(this.processId).subscribe(data => {
       console.log(data);
+      this.process = data;
     }, error => {
       this.toastr.error('There was an error while getting the data for business process');
+    });
+  }
+
+  private getPossibleReviewers(): void {
+    this.businessProcessService.recommendReviewers(this.processId).subscribe(data => {
+      this.possibleReviewers = data.reverse();
+    }, error => {
+      this.toastr.error('There was an error while making reviewer recommendation.');
+    });
+  }
+
+  addReviewerToList(id: string): void {
+    const elementIndex = this.possibleReviewers.findIndex(element => element.id === id);
+    const reviewer = this.possibleReviewers[elementIndex];
+    this.possibleReviewers.splice(elementIndex, 1);
+    this.choosenReviewers.push(reviewer);
+  }
+
+  removeReviewerFromList(id: string): void {
+    const elementIndex = this.choosenReviewers.findIndex(element => element.id === id);
+    const reviewer = this.choosenReviewers[elementIndex];
+    this.choosenReviewers.splice(elementIndex, 1);
+    this.possibleReviewers.push(reviewer);
+  }
+
+  addReviewersToProcess(): void {
+    if (this.choosenReviewers.length === 0) {
+      this.toastr.warning('Please choose at least 1 reviewer.');
+      return;
+    }
+    
+    const userIds: string[] = this.choosenReviewers.map(reviewer => reviewer.id);
+
+    this.businessProcessService.addReviewers(this.processId, userIds).subscribe(data => {
+      this.toastr.success('Reviewers have been successfully added.');
+    }, error => {
+      this.toastr.error('Error while adding reviewers to the business process.');
     });
   }
 }
--- xws-client/src/app/services/business-process.service.ts
+++ xws-client/src/app/services/business-process.service.ts
@@ -1,5 +1,5 @@
 import { Observable } from 'rxjs';
-import { API_BUSINESS_PROCESSES, API_SHOW_MY_REVIEW_REQUESTS, API_ACCEPT_REVIEW_REQUEST, API_DECLINE_REVIEW_REQUEST, API_ACCEPT_PUBLICATION, API_DECLINE_PUBLICATION } from './../config/api-paths';
+import { API_BUSINESS_PROCESSES, API_SHOW_MY_REVIEW_REQUESTS, API_ACCEPT_REVIEW_REQUEST, API_DECLINE_REVIEW_REQUEST, API_ACCEPT_PUBLICATION, API_DECLINE_PUBLICATION, API_RECOMMEND_REVIEWERS, API_ADD_REVIEWERS } from './../config/api-paths';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
@@ -37,5 +37,13 @@ export class BusinessProcessService {
 
   declinePublication(processId: string): Observable<any> {
     return this.http.get(`${API_DECLINE_PUBLICATION}/${processId}`);
+  }
+
+  recommendReviewers(processId: string): Observable<any> {
+    return this.http.get(`${API_RECOMMEND_REVIEWERS}/${processId}`);
+  }
+
+  addReviewers(processId: string, userIds: string[]): Observable<any> {
+    return this.http.put(`${API_ADD_REVIEWERS}/${processId}`, userIds);
   }
 }
